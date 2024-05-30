c78f0bc85785ce555445b3c0edd11b1098d9dbe6
MuhammadRadifa <m.difa123456@gmail.com>
2024-05-30 18:57:16 +0800

    feat : add prompt template

--- app/src/main/java/com/gemini/mypromptai/ui/screen/MainActivity.kt
+++ app/src/main/java/com/gemini/mypromptai/ui/screen/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.gemini.mypromptai.ui.screen
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,6 +11,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -20,7 +24,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gemini.mypromptai.ui.components.BottomBar
 import com.gemini.mypromptai.ui.components.CardMessage
+import com.gemini.mypromptai.ui.components.CardTemplate
+import com.gemini.mypromptai.ui.components.TopBar
 import com.gemini.mypromptai.ui.data.ResultState
+import com.gemini.mypromptai.ui.data.promptTemplate
 import com.gemini.mypromptai.ui.theme.MyPromptAITheme
 import com.gemini.mypromptai.ui.theme.primaryBackground
 import com.gemini.mypromptai.ui.theme.textColor
@@ -43,52 +50,68 @@ class MainActivity : ComponentActivity() {
                             }
                         )
                     },
+                    topBar = {
+                        TopBar(
+                            clearText = {
+                                viewModel.clearChat()
+                            },
+                            text = "MyPromptAI"
+                        )
+                    },
                     contentColor = textColor,
                     containerColor = primaryBackground
                 ) { innerPadding ->
 
-                    viewModel.response.collectAsState().value.let {
+                    viewModel.response.collectAsState(
+                        initial = ResultState.Success(emptyList())
+                    ).value.let {
                         data ->
                         when (data) {
                             is ResultState.Success -> {
-                                LazyColumn(
-                                    modifier = Modifier
-                                        .fillMaxSize()
-                                        .padding(innerPadding)
-                                        .padding(horizontal = 12.dp),
-                                    verticalArrangement = Arrangement.Bottom
+                                if(data.data.isEmpty()){
+                                    LazyVerticalGrid(
+                                        modifier = Modifier
+                                            .fillMaxSize()
+                                            .padding(innerPadding)
+                                            .padding(horizontal = 14.dp),
+                                        verticalArrangement = Arrangement.Bottom,
+                                        columns = GridCells.Fixed(2)
+                                    ) {
+                                        items(promptTemplate){
+                                            template ->
+                                            CardTemplate(
+                                                text = template.text,
+                                                icon = template.icon,
+                                                onSend={
+                                                    viewModel.sendPrompt(template.prompt)
+                                                    viewModel.storeUserPrompt(template.prompt)
+                                                }
+                                            )
+                                        }
+                                    }
+                                }else{
+                                    LazyColumn(
+                                        modifier = Modifier
+                                            .fillMaxSize()
+                                            .padding(innerPadding)
+                                            .padding(horizontal = 12.dp),
+                                        verticalArrangement = Arrangement.Bottom
 
-                                ) {
-                                    items(data.data){
-                                        CardMessage(text = it.message!!, isUser = it.isUser)
+                                    ) {
+                                        items(data.data){
+                                            CardMessage(text = it.message!!, isUser = it.isUser)
+                                        }
                                     }
                                 }
+
                             }
 
                             is ResultState.Loading -> {
-                                Column(
-                                    modifier = Modifier
-                                        .fillMaxSize()
-                                        .padding(innerPadding)
-                                        .padding(horizontal = 12.dp),
-                                    verticalArrangement = Arrangement.Bottom
-
-                                ) {
-                                    Text(text = "Loading...")
-                                }
+                                Log.i("MainActivity", "Loading")
                             }
 
                             is ResultState.Error -> {
-                                Column(
-                                    modifier = Modifier
-                                        .fillMaxSize()
-                                        .padding(innerPadding)
-                                        .padding(horizontal = 12.dp),
-                                    verticalArrangement = Arrangement.Bottom
-
-                                ) {
-                                    Text(text = "Error: ${data.error}")
-                                }
+                                Log.e("MainActivity", "Error: $data")
                             }
                         }
                     }
